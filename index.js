843fa30081a3242e08fc0336e5e0f7294c07ab01
Anandam Siva <sivadevops80123@gmail.com>
2022-10-22 01:02:14 +0530

    Iterations

---
+++ index.js
@@ -0,0 +1,60 @@
+// for adding  digits in a number
+// let g = "12";
+// g= Number(g);
+// console.log(typeof(g))
+
+// let x = 12345678991;
+// console.log(x)
+// x= String(x)
+// console.log(typeof(x))
+// let result=0 ;
+// for (i = 0; i< x.length  ; i++) {
+//   let z = x[i];
+//   z=Number(z)
+//   result = result + z ;
+// }
+// console.log(result);
+
+
+//making of right angle triangle
+
+// let x=10;
+// let result = " "
+// for (i=1; i<10; i++){
+//   result= "*" +result;
+//   console.log(result)
+// }
+// console.log(result)
+
+
+
+// making of triangle outline with "*"
+// function pyramid(n) {
+//   let y= " ".repeat(n)+"*"+" ".repeat(n)
+// console.log(y)
+//   for (let i = 1; i <= n; i++) {
+
+//     let str = " ".repeat(n - i)
+//     let str2 = " ".repeat(2 * i - 1)
+//     console.log(str + "*" + str2 + "*" + str);
+//   }
+//   let x = ("*"+" ").repeat(n + 1);
+//   console.log(x)
+// }
+// pyramid(12);
+
+
+
+
+// making of triangle  with "*"
+// function pyramid(n) {
+
+//     for (let i = 1; i <= n; i++) {
+
+//         let str = " ".repeat(n - i)
+//         let str2 = "*".repeat(2 * i - 1)
+
+//         console.log(str + str2 + str);
+//     }
+// }
+// pyramid(5);
